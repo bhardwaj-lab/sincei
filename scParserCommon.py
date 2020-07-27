3cb537646e631532f0bc45e3dc2a00b818460da3
vivekbhr <v.bhardwaj@hubrecht.eu>
2020-07-27 19:49:23 +0200

    start moving common options to parser

---
+++ scParserCommon.py
@@ -0,0 +1,59 @@
+import argparse
+import os
+from scDeepTools._version import __version__
+
+def output(args=None):
+    parser = argparse.ArgumentParser(add_help=False)
+    group = parser.add_argument_group('Output')
+    group.add_argument('--outFilePrefix', '-o',
+                       help='Output file name prefix.',
+                       metavar='FILEPREFIX',
+                       type=int,
+                       required=True)
+
+    group.add_argument('--outFileFormat', '-of',
+                       help='Output file type. Either "bigwig" or "bedgraph".',
+                       choices=['bigwig', 'bedgraph'],
+                       default='bigwig')
+
+    return parser
+
+def filterOptions(args=None):
+    parser = argparse.ArgumentParser(add_help=False)
+    group = parser.add_argument_group('Filter')
+
+    group.add_argument('--tagName', '-tn',
+                          metavar='STR',
+                          help='Name of the BAM tag from which to extract barcodes.',
+                          type=str,
+                          default='BC')
+
+    group.add_argument('--motifFilter', '-m',
+                          metavar='STR',
+                          help='Check whether a given motif is present in the read and the corresponding reference genome. '
+                                'This function checks for the motif at the 5-end of the read and at the 5-overhang in the genome. '
+                                'Reads not containing the given motif are not discarded. ',
+                          type=str,
+                          nargs='+',
+                          default=None)
+
+    group.add_argument('--genome2bit', '-g',
+                          metavar='STR',
+                          help='If --motifFilter is provided, please also provide the genome sequence (in 2bit format). ',
+                          type=str,
+                          default=None)
+
+    group.add_argument('--GCcontentFilter', '-gc',
+                          metavar='STR',
+                          help='Check whether the GC content of the read falls within the provided range. '
+                                'If the GC content of the reads fall outside the range, they are discarded. ',
+                          type=str,
+                          default=None)
+
+    group.add_argument('--minAlignedFraction',
+                           help='Minimum fraction of the reads which should be aligned to be counted. This includes '
+                           'mismatches tolerated by the aligners, but excludes InDels/Clippings (Default: %(default)s)',
+                           metavar='FLOAT',
+                           default=None,
+                           type=float,
+                           required=False)
