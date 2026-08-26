64eb8287895a9915a5e5a290547e7866ec48feb1
Sancho Gómez <fer.sancho.gom@gmail.com>
2026-08-26 12:12:58 +0200

    Prepare for merge into sincei

--- sincei/__init__.py
+++ sincei/__init__.py
@@ -4,10 +4,6 @@ from sincei import _sincei as internal
 
 from . import plotting as pl
 
-# from . import export as ex
-# from . import preprocessing as pp
-# from . import tools as tl
-
 __version__ = internal.version()
 
-__all__ = ["ex", "pl", "pp", "tl"]
+__all__ = ["pl"]
