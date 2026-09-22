e09e32ba74c2bd051f833f45103ae1d6f9d1ef8c
Sancho Gómez <fer.sancho.gom@gmail.com>
2026-09-22 15:46:32 +0200

    Split coverage.rs to match crate structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub mod annotation;
 pub mod bam;
+mod bulk_coverage;
 mod count_reads;
 pub mod counting;
 mod filter_barcodes;
@@ -42,7 +43,7 @@ fn _sincei(_py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(count_reads::count_features, m)?)?;
 
     // Export
-    m.add_function(wrap_pyfunction!(counting::coverage::bulk_coverage, m)?)?;
+    m.add_function(wrap_pyfunction!(bulk_coverage::bulk_coverage, m)?)?;
 
     // Genome annotation parsing
     m.add_function(wrap_pyfunction!(
